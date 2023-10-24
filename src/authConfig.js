5871ed15a05089e9847dc9240112da94e59feb23
sohel5G <105277301+sohel5G@users.noreply.github.com>
2023-10-24 16:24:47 +0600

    env file added

--- src/authConfig.js
+++ src/authConfig.js
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDQXng7fafpELFF7ILu4ywZvd2Bso7zpCo",
-    authDomain: "coffee-store-4827b.firebaseapp.com",
-    projectId: "coffee-store-4827b",
-    storageBucket: "coffee-store-4827b.appspot.com",
-    messagingSenderId: "151006226076",
-    appId: "1:151006226076:web:91c39f9c8c8bcf1e8b2f59"
-
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
@@ -16,4 +15,3 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 export default auth;
-
